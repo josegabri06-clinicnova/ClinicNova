e5e9c116d15408b1afa879892466a2381d46e2c9
Jose Gabriel Carrillo Martinez <josegabriel@iMac-de-Jose.local>
2025-11-20 13:50:35 +0100

    Eliminar franja negra entre secciones

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -1,5 +1,15 @@
 export default function SectionDivider() {
   return (
-    <div className="w-full h-20 md:h-28 bg-transparent"></div>
+    <div
+      className="
+        w-full
+        h-10
+        md:h-16
+        bg-gradient-to-b
+        from-transparent
+        via-[#f3f5ff]
+        to-white
+      "
+    />
   );
 }
